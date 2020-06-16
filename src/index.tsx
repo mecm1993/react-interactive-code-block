a01fd0322f7b4ed4d0ea0158aeb0b2565177e3fa
Manuel Cepeda <mecm1993@gmail.com>
2020-06-15 21:14:01 -0400

    feat: add copy and minor changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,36 +1,36 @@
 import * as React from 'react'
 import styles from './styles.module.css'
+import copyImg from './assets/copy.png'
 
 interface CodeBlockProps {
   message: string,
   typing?: boolean,
   speed?: number,
+  icon?: string,
   className?: string,
   style?: React.CSSProperties,
 }
 
 export const CodeBlock = (codeBlockProps: CodeBlockProps) => {
-  const [text, setText] = React.useState('');
-  const fullText: string = codeBlockProps.message;
-  const speed: number = codeBlockProps.speed ? codeBlockProps.speed : 30;
+  const _message: string = codeBlockProps.message;
+  const _typing: boolean = codeBlockProps.typing ? codeBlockProps.typing : false;
+  const _speed: number = codeBlockProps.speed ? codeBlockProps.speed : 30;
+  const text = useTyping(_message, _typing, _speed);
 
+  return renderComponent(text, codeBlockProps.icon, codeBlockProps.className, codeBlockProps.style);
+}
+
+const useTyping = (message: string, isTyping: boolean, speed: number) => {
+  const [text, setText] = React.useState('');
   React.useEffect(() => {
-    if (codeBlockProps.typing) {
-      handleTyping(setText, fullText, text, speed);
+    if (isTyping) {
+      handleTyping(setText, message, text, speed);
     } else {
-      setText(fullText);
+      setText(message);
     }
   }, [text]);
 
-  return getHtml(text, codeBlockProps.className, codeBlockProps.style);
-}
-
-const getHtml = (text: string, className?: string, style?: React.CSSProperties) => {
-  const _className: string = className ? className : '';
-  const _style: React.CSSProperties = style ? style : {};
-  return <div className={styles.block + ' ' + _className} style={_style}>
-      {text}
-    </div>
+  return text;
 }
 
 const handleTyping = (setText: Function, message: string, text: string, speed: number) => {
@@ -38,4 +38,32 @@ const handleTyping = (setText: Function, message: string, text: string, speed: n
     setText(message.substring(0, text.length + 1));
   }, speed);
   return () => clearTimeout(timer);
+}
+
+const renderComponent = (text: string, icon?: string, className?: string, style?: React.CSSProperties) => {
+  const _className: string = className ? className : '';
+  const _style: React.CSSProperties = style ? style : {};
+  return <div className={styles.block + ' ' + _className} style={_style}>
+      {text}
+      <div className={styles.actions}>
+        <button
+          className={styles.btn}
+          onClick={() => onCopyPress(text)}
+        >
+          <img
+            className={styles.img}
+            src={icon ? icon : copyImg}
+          />
+        </button>
+      </div>
+    </div>
+}
+
+const onCopyPress = (text: string) => {
+  const textField = document.createElement('textarea');
+  textField.innerText = text;
+  document.body.appendChild(textField);
+  textField.select();
+  document.execCommand('copy');
+  textField.remove();
 }
